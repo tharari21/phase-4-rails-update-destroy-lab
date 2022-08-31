34c4718ecdffb57782e12b69617c96a3c49799f6
Tomer Harari <tharari21@gmail.com>
2022-08-31 14:36:24 -0400

    commit tree df30db2e853ed979d063444e05bd6234beafd915

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
@@ -17,9 +17,30 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def update
+    plant = find
+    if plant.update(plant_params)
+      render json: plant, status: 204
+    else
+      render json: {error: "Not Updated"}
+    end
+  end
+  def destroy
+    plant = find
+    if plant.destroy
+      head :no_content
+    else
+      render json: {error: "Could Not Delete"}
+    end
+  end
 
   private
-
+  def find
+    Plant.find(params[:id])
+  end
+  def render_not_found_response
+    render json: {error: "Plant Not Found"}, status: :not_found
+  end
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
